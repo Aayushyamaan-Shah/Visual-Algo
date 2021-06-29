5db19d51ceb3b7e128634cdaba53a7d7d876e5dc
A5Playz <aayushs474@gmail.com>
2021-06-30 04:59:01 +0530

    Added initial comments to visualize.js

--- visualize/js/visualize.js
+++ visualize/js/visualize.js
@@ -1,3 +1,6 @@
+
+// Declaration and initialization of all gobal variables
+
 selectedPos1 = 1;
 selectedPos2 = 2;
 elementCount = 5;
@@ -8,27 +11,32 @@ speedSelector = 4;
 sortingSpeed = [2.5, 10, 50, 250, 500];
 const sleep = (milliseconds) => {
     return new Promise(resolve => setTimeout(resolve, milliseconds))
-  }
-
-function randomInt(min, max){
-    return Math.ceil((Math.random() * (max-min)) + min);
 }
 
-function initArr(){
-    for(let k = 0; k < elementCount; k++){
-        elementsArr.push(randomInt(5,999));
+// Function to create random integer between the given range
+function randomInt(min, max) {
+    return Math.ceil((Math.random() * (max - min)) + min);
+}
+
+// Function to initialize the array with random values
+function initArr() {
+    for (let k = 0; k < elementCount; k++) {
+        elementsArr.push(randomInt(5, 999));
     }
     elementsMax = Math.max(...elementsArr);
     elementsMin = Math.min(...elementsArr);
     console.log(elementsArr)
 }
 
-function clearDOMElements(){
+// Function to cleares old elements
+function clearDOMElements() {
     document.querySelector(`.elementsContainer`).innerHTML = '';
 }
 
-function addDOMElement(val){
+// Function to create and add new bars in the visualizer
+function addDOMElement(val) {
 
+    // FORMAT:
     // <div class="swapper default">
     //     <p>wot</p>
     // </div>
@@ -38,75 +46,82 @@ function addDOMElement(val){
     newDiv.classList.add("default");
     newDiv.style.height = (val * 90) / elementsMax + "%";
     newDiv.style.width = document.querySelector(`.elementsContainer`).clientWidth / elementCount + "%";
-    newDiv.innerHTML = "<p>"+val+"</p>";
+    newDiv.innerHTML = "<p>" + val + "</p>";
     document.querySelector(`.elementsContainer`).appendChild(newDiv);
 }
 
-function initElements(){
-    for(let i = 0; i < elementCount; i++){
+// Function to iteratively add all the elements in the sorting div
+function initElements() {
+    for (let i = 0; i < elementCount; i++) {
         addDOMElement(elementsArr[i]);
     }
 }
 
-async function swapDom(el1, el2){
+// Function to swap 2 HTML elements
+async function swapDom(el1, el2) {
     let cloneEl1 = el1.cloneNode(true);
     let cloneEl2 = el2.cloneNode(true);
-    
-    el1.parentNode.replaceChild(cloneEl2,el1);
-    el2.parentNode.replaceChild(cloneEl1,el2);
+
+    el1.parentNode.replaceChild(cloneEl2, el1);
+    el2.parentNode.replaceChild(cloneEl1, el2);
 }
 
-function selectDOM(element){
+// Function to apply color when the element is selected
+function selectDOM(element) {
     element.classList.remove('default');
     element.classList.add('selected');
 }
 
-function unselectDOM(element){
+// Function to remove the color when the element is no longer selected
+function unselectDOM(element) {
     element.classList.remove('selected');
     element.classList.add('default');
 }
 
-async function sortElements(){
+// Function that sorts the elements using simple sort
+async function sortElements() {
     let i = 0;
     let j = 0;
     console.log("Sorting elements");
-    for(i = 0; i < elementCount-1; i++){
-        for(j = i+1; j < elementCount; j++){
+    for (i = 0; i < elementCount - 1; i++) {
+        for (j = i + 1; j < elementCount; j++) {
 
-            let child1 = document.querySelector(`.swapper:nth-child(${i+1})`);
-            let child2 = document.querySelector(`.swapper:nth-child(${j+1})`);
-            
+            let child1 = document.querySelector(`.swapper:nth-child(${i + 1})`);
+            let child2 = document.querySelector(`.swapper:nth-child(${j + 1})`);
+
             selectDOM(child1);
             selectDOM(child2);
-            console.log(sortingSpeed[5-speedSelector])
-            await sleep(sortingSpeed[5-speedSelector]);
+            console.log(sortingSpeed[5 - speedSelector])
+            await sleep(sortingSpeed[5 - speedSelector]);
 
-            if(elementsArr[i] > elementsArr[j]){
+            if (elementsArr[i] > elementsArr[j]) {
 
                 let temp = elementsArr[j];
                 elementsArr[j] = elementsArr[i];
                 elementsArr[i] = temp;
 
-                swapDom(child1,child2);
-            } 
-            
-            child1 = document.querySelector(`.swapper:nth-child(${i+1})`);
-            child2 = document.querySelector(`.swapper:nth-child(${j+1})`);
+                swapDom(child1, child2);
+            }
+
+            child1 = document.querySelector(`.swapper:nth-child(${i + 1})`);
+            child2 = document.querySelector(`.swapper:nth-child(${j + 1})`);
             unselectDOM(child1);
             unselectDOM(child2);
 
         }
-        
+
     }
     console.log("OUF");
     console.log(elementsArr)
 }
 
-function updateSpeed(){
+// Function that updates the speed
+function updateSpeed() {
     speedSelector = document.querySelector(`#speed`).value;
 }
 
-function primer(){
+// Function that calls all the intializing functions
+function primer() {
     elementCount = parseInt(document.getElementById('elementCount').value);
     console.log(elementCount);
     elementsArr = [];
@@ -115,7 +130,8 @@ function primer(){
     initElements();
 }
 
-function sorter(){
+// Function that calls the sorting function
+function sorter() {
 
     document.getElementById('clicker').disabled = true;
     document.getElementById('sortClicker').disabled = true;
